dc4b4158712b0a442a74b941fb91ef37c4bd24b2
chaitanyaenr <naga.ellluri@colorado.edu>
2016-02-15 10:23:37 -0700

    changed interfaces

--- monitor.py
+++ monitor.py
@@ -1,6 +1,6 @@
 import subprocess
 import time
-INTERFACES = [ 1, 2, 3, 4, 5,6, 7, 9, 10, 11 ]
+INTERFACES = [ 1, 2, 3,6, 7, 9, 10,11 ]
 
 DEVICES={ "ifInOctets": ".1.3.6.1.2.1.2.2.1.10", "ifInUcastPkts": ".1.3.6.1.2.1.2.2.1.11", "ifInUcastPkts": "1.3.6.1.2.1.2.2.1.12", "ifOutOctets": "1.3.6.1.2.1.2.2.1.16", "ifOutUcastPkts": "1.3.6.1.2.1.2.2.1.17", "ifOutNUcastPkts": ".1.3.6.1.2.1.2.2.1.18"}
 def snmpdxd():
