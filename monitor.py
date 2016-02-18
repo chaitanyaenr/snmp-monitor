398822e55753d7bed822c87d30f323a223220e74
chaitanyaenr <naga.ellluri@colorado.edu>
2016-02-17 20:20:42 -0700

    updated program

--- monitor.py
+++ monitor.py
@@ -1,11 +1,12 @@
 import subprocess
 import time
 INTERFACES = [ 1, 2, 3,6, 7, 9, 10,11 ]
-
-DEVICES={ "ifInOctets": ".1.3.6.1.2.1.2.2.1.10", "ifInUcastPkts": ".1.3.6.1.2.1.2.2.1.11", "ifInUcastPkts": "1.3.6.1.2.1.2.2.1.12", "ifOutOctets": "1.3.6.1.2.1.2.2.1.16", "ifOutUcastPkts": "1.3.6.1.2.1.2.2.1.17", "ifOutNUcastPkts": ".1.3.6.1.2.1.2.2.1.18"}
+COUNT=[1,2,3,4,5,6,7,8,9,10,11]
+DEVICES={ "ifInOctets": ".1.3.6.1.2.1.2.2.1.10", "ifInUcastPkts": ".1.3.6.1.2.1.2.2.1.11", "ifInNUcastPkts": "1.3.6.1.2.1.2.2.1.12", "ifOutOctets": "1.3.6.1.2.1.2.2.1.16", "ifOutUcastPkts": "1.3.6.1.2.1.2.2.1.17", "ifOutNUcastPkts": ".1.3.6.1.2.1.2.2.1.18"}
 def snmpdxd():
 	for devices in DEVICES:
-		print (devices) 
+		print (devices)
+		print "//////////////////////////////////" 
 		for interfaces in INTERFACES:
 			command=["/usr/bin/snmpget",
               			 "-v", "2c",
@@ -15,12 +16,13 @@ def snmpdxd():
 
 			output = subprocess.check_output(command)
                	 	output = output.split()
-                	print output[-1]           
+                	print output[-1]
+			           
 	return
 
 
-for i in INTERFACES:
+for i in COUNT:
 	print (i)
-	print "////////////////////"
+	print "***************************"
 	snmpdxd();
 	time.sleep(600)
